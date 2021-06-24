8453e4f10a47eaffdca618d3ca5c8ac58cdbf373
Eduardo-Puentes <eduardo.puentes.dev@gmail.com>
2021-06-24 18:06:12 -0500

    Working download

--- downloader.py
+++ downloader.py
@@ -1,1 +1,11 @@
-hi
+import requests
+image_url = "https://cdn.pixabay.com/audio/2020/11/10/audio_547ebbf828.mp3"
+
+r = requests.get(image_url)
+
+filename = "song1"
+
+route = "files/" + "filename" + ".mp3"
+
+with open(route, 'wb') as f:
+    f.write(r.content)
