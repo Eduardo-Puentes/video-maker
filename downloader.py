bdab4dff17b505c45704a1bf4f704d96f0fc359e
Eduardo-Puentes <eduardo.puentes.dev@gmail.com>
2021-06-27 20:35:08 -0500

    Load and download functions setted

--- downloader.py
+++ downloader.py
@@ -2,34 +2,62 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
-for i in range(2):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument("--test-type")
-    driver = webdriver.Chrome(
-        executable_path=r'chromedriver_win3\chromedriver.exe', chrome_options=options)
-    driver.get(
-        'https://pixabay.com/music/search/mood/relaxing/?page=' + str((i + 1)))
 
-    html = driver.page_source
+def main():
 
-    soup = BeautifulSoup(html, "lxml")
+    # load all the links of the songs
+    songs_links = load_songs()
+    print("There are " + str(len(songs_links)) + " songs to download")
 
-    songs = soup.find_all("input", class_="track-source")
+    # download the songs by his link
+    download_songs(songs_links)
 
+
+def load_songs():
+    go = True
+    page = 1
     links = []
 
-    for song in songs:
-        links.append(song.get("value"))
+    while go:
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument("--test-type")
+        driver = webdriver.Chrome(
+            executable_path=r'chromedriver_win3\chromedriver.exe', chrome_options=options)
+        driver.get(
+            'https://pixabay.com/music/search/mood/relaxing/?page=' + str(page))
 
-for i in range(len(links)):
-    r = requests.get(links[i])
+        html = driver.page_source
 
-    filename = "song" + str(i)
+        soup = BeautifulSoup(html, "lxml")
 
-    route = "files/" + filename + ".mp3"
+        songs = soup.find_all("input", class_="track-source")
 
-    with open(route, 'wb') as f:
-        f.write(r.content)
+        if not songs:
+            go = False
 
-    print("song " + str(i) + " ready!")
+        for song in songs:
+            links.append(song.get("value"))
+            print(song.get("value"))
+
+        page = page + 1
+
+    return links
+
+
+def download_songs(links):
+    for i in range(len(links)):
+        r = requests.get(links[i])
+
+        filename = "song" + str(i)
+
+        route = "files/" + filename + ".mp3"
+
+        with open(route, 'wb') as f:
+            f.write(r.content)
+
+        print("song " + str(i) + " ready!")
+
+
+if __name__ == "__main__":
+    main()
